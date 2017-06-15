29b720cee482a6ec4140b4a5eb0e0ba867594e7f
Jan Dockx <jandockx@gmail.com>
2017-06-15 17:18:45 +0200

    Fix require path

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
 const program = require("commander");
 const GitInfo = require("./GitInfo");
-const tagGitRepo = require("./js/tagGitRepo");
+const tagGitRepo = require("./tagGitRepo");
 const packageVersion = require("pkginfo")(module, "version");
 
 //noinspection JSCheckFunctionSignatures
